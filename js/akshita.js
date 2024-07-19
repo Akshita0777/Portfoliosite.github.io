07ffa8a1054a95af95f2ed36ca2e9736d07a851a
Akshita0777 <akshitagupta0777@gmail.com>
2024-07-20 00:52:55 +0530

    my portfolio site

---
+++ js/akshita.js
@@ -0,0 +1,13 @@
+ const menuIcon = document.querySelector('#menu-icon');
+ const navbar = document.querySelector('.navbar');
+
+ menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+ }
+
+
+
+
+ 
